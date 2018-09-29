0e8f2ae3a564aaff0f46eb2f6454165aee49fcc9
hannyle <hannyle91@gmail.com>
2018-09-29 11:27:50 +0300

    add redirect for /

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Link, Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Link, Switch, Redirect} from 'react-router-dom';
 import Beer from './components/Beers';
 import Home from './components/Home';
 import './App.css';
+
 
 class App extends Component {
   render(){
@@ -11,6 +12,7 @@ class App extends Component {
       <BrowserRouter>                 
           <Switch>            
             <Route path="/home" component={Home}/>
+            <Redirect from="/" to="/home"/>
             <Route path={"/beer/:id"} component={Beer}/>
           </Switch>        
       </BrowserRouter>
